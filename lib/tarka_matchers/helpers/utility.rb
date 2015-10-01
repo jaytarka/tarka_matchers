89520f5b902e34c83f18db1436c5863f33085df9
jaytarka <jaytarka@hotmail.com>
2015-10-02 00:53:40 +0100

    using utilize with have_an_instance_variable_of matcher

--- lib/tarka_matchers/helpers/utility.rb
+++ lib/tarka_matchers/helpers/utility.rb
@@ -1,23 +1,27 @@
 module TarkaMatchers
 	module Helpers
 		module Utility
-			attr_reader :failure_message, :description
+			attr_reader :description, :failure_message, :failure_message_when_negated
 
 			def pass_default message
-				@pass_default = message
+				@description = message
 			end
 
 			def fail_default message
-				@fail_default = message
+				@failure_message = message
+			end
+	
+			def negated_default message=nil
+				@failure_message_when_negated = message || "did #{@description}"
 			end
 
 			def pass_with_message message=nil
-				@description = message || @pass_default
+				@description = message if message
 				true
 			end
 
 			def fail_with_message message=nil
-				@failure_message = message || @fail_default
+				@failure_message = message if message
 				false
 			end
 
--- lib/tarka_matchers/matchers/class/have_an_instance_variable_of.rb
+++ lib/tarka_matchers/matchers/class/have_an_instance_variable_of.rb
@@ -1,4 +1,5 @@
 require 'tarka_matchers/helpers/expectation/result'
+require 'tarka_matchers/helpers/utility'
 require 'tarka_matchers/formatters/difference'
 module TarkaMatchers
 	module Matchers
@@ -8,6 +9,7 @@ module TarkaMatchers
 			end
 
 			class HaveAnInstanceVariableOf
+				include TarkaMatchers::Helpers::Utility
 				def initialize instance_name
 					@instance_name = instance_name
 				end
@@ -18,22 +20,23 @@ module TarkaMatchers
 				end
 				
 				def matches? actual
-#					fail_with "failed to contain an instance variable called '@blarsey'. It does not exist inside the class." unless actual.instance_variable_defined?(@instance_name)
 					@actual = actual.instance_variable_get(@instance_name)
+					fail_with_message "failed to contain an instance variable called '#{@instance_name}'. It does not exist inside the class." unless actual.instance_variable_defined?(@instance_name)
+					pass_default "contain an instance variable called, '#{@instance_name}', that equals '#{@expected}'."
+					negated_default
+					#fail_default "failed to #{description}\n#{TarkaMatchers::Formatters::Difference.difference(@expected,@actual)}"
 					@actual == @expected
 				end
 
-				def description
-					"contain an instance variable called, '#{@instance_name}', that equals '#{@expected}'."
-				end
+#				def description
+#				end
 
-				def failure_message
-					"failed to #{description}\n#{TarkaMatchers::Formatters::Difference.difference(@expected,@actual)}"
-				end
+#				def failure_message
+#				end
 
-				def failure_message_when_negated
-					"did #{description}"
-				end
+#				def failure_message_when_negated
+#					"did #{description}"
+#				end
 			end
 		end
 	end
