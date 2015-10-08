5303cf8cf69c0f4aa469485830a2f93369a01660
jaytarka <jaytarka@hotmail.com>
2015-10-08 17:16:34 +0100

    fix seg fault

--- spec/support/methods/mock_formatter.rb
+++ spec/support/methods/mock_formatter.rb
@@ -1,8 +1,11 @@
 def mock_formatter klass, method_name, mocked_message
+	ap "FORMATTER"
 	clone_name = :c
-	klass.define_singleton_method(clone_name, klass.singleton_method(method_name).unbind)
-	klass.instance_variable_set(:@x, [mocked_message,clone_name])
-	klass.define_singleton_method(method_name) do |expected,actual|
-		caller[0].split('').last(9).join == "matches?'" ? @x[0] : self.send(@x[1],expected,actual)
+	unless klass.methods.include?(clone_name)
+		klass.define_singleton_method(clone_name, klass.singleton_method(method_name).unbind)
+		klass.instance_variable_set(:@x, [mocked_message,clone_name])
+		klass.define_singleton_method(method_name) do |expected,actual|
+			caller[0].split('').last(9).join == "matches?'" ? @x[0] : self.send(@x[1],expected,actual)
+		end
 	end
 end
